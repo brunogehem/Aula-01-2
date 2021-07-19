597501dc7860b5c176d6ba58ad9f6747eaf0a348
Bruno Gehem De Moura <gehembruno@gmail.com>
2021-07-19 15:56:58 -0300

    Projeto inicial aula 01

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		//As linhas abaixo usam o println para exibir uma mensagem na tela
+		System.out.println("Hello World");
+		System.out.println("Lá vem ele");
+		
+	}
+
+}
